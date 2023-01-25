281f8bba5c7e00eaa974cbef7057f659a5e843b3
johannes-lindgren <johannes-lindgren@hotmail.com>
2023-01-25 18:00:42 +0100

    Counting correct answers. Fixed bugs. Reveal only when all answers questions are answered.

--- src/components/QuizGame.vue
+++ src/components/QuizGame.vue
@@ -5,13 +5,15 @@
     </el-header>
     <el-main>
       <div class="questions">
+        <div>You answered: {{ givenAnswers.length }} / {{ questions.length }} questions</div>
         <div
-          v-for="question in questions"
-          :key="question.id"
+            v-for="question in questions"
+            :key="question.id"
         >
-            <QuestionCard :question="question" @on-answer="onAnswer" />
+          <QuestionCard :question="question" @on-answer="onAnswer"/>
         </div>
-        <div>Score: {{ correctAnswersCount }} / {{ questions.length }}</div>
+        <div v-if="givenAnswers.length < questions.length">Waiting for you to answer all questions...</div>
+        <div v-else>Score: {{ correctAnswersCount }} / {{ questions.length }}</div>
       </div>
     </el-main>
     <el-footer>By Storyblok - Group D</el-footer>
@@ -20,39 +22,36 @@
 
 <script>
 import QuestionCard from "./QuestionCard.vue";
-import { fetchQuestions } from "../fetchQuestions";
-import { computed, ref, watch } from "vue-demi";
+import {fetchQuestions} from "../fetchQuestions";
+import {computed, ref, watch} from "vue-demi";
 
 export default {
-  components: { QuestionCard },
+  components: {QuestionCard},
   async setup() {
-    const questions = await fetchQuestions(10);
+    const questions = await fetchQuestions(5);
     const givenAnswers = ref([]);
     const givenAnswer = (question) =>
-      givenAnswers.value.find((givenAnswer) => question.id === givenAnswer.id);
-    function onAnswer(answer) {
-      if (
-        givenAnswers.value.find((givenAnswer) => answer.id === givenAnswer.id)
-      ) {
-        // Do nothing
-        return;
-      }
-      givenAnswers.value.push(answer);
-      console.log(givenAnswers.value);
-    }
+        givenAnswers.value.find((givenAnswer) => question.id === givenAnswer.id);
     const questionById = (id) =>
-      questions.find((question) => question.id === id);
-    const isCorrect = (question, answer) => question.correctAnswer === answer;
+        questions.find((question) => question.id === id);
+
+    function onAnswer(answer) {
+      givenAnswers.value = [
+        ...givenAnswers.value.filter(givenAnswer => givenAnswer.id !== answer.id),
+        answer
+      ]
+    }
+
+    const isCorrect = (question, givenAnswer) =>  question.correctAnswer === givenAnswer.answer;
     const correctAnswersCount = computed(
-      () =>
-        givenAnswers.value.filter((givenAnswer) =>
-          isCorrect(questionById(givenAnswer.id), givenAnswer)
+        () => givenAnswers.value.filter((givenAnswer) =>
+            isCorrect(questionById(givenAnswer.id), givenAnswer)
         ).length
     );
     watch(correctAnswersCount, () => {
-      localStorage('correctAnswersCount', correctAnswersCount)
+      console.log('correctAnswersCount', correctAnswersCount)
     })
-    return { questions, onAnswer, givenAnswers, givenAnswer, correctAnswers: correctAnswersCount };
+    return {questions, onAnswer, givenAnswers, givenAnswer, correctAnswersCount};
   },
 };
 </script>
